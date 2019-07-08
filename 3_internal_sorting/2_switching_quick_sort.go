a9ea6170e00d9256d2f5f704328ecab477a4d6bf
solozyx <zhaoyingxin@aliyun.com>
2019-07-08 22:40:34 +0800

    交换式排序 - 快速排序

---
+++ 3_internal_sorting/2_switching_quick_sort.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// 交换式排序 - 快速排序
+
+// 1. left 表示 数组左边的下标
+// 2. right 表示数组右边的下标
+// 3. array 表示要排序的数组
+func QuickSort(left int, right int, array *[80000000]int) {
+	l := left
+	r := right
+	pivot := array[(left+right)/2]
+
+	temp := 0
+
+	// for 循环目标,将比 pivot 小的数放到左边, 比 pivot 大的数放到右边
+	for l < r {
+		// 从 pivot 的左边找到大于等于pivot的值
+		for array[l] < pivot {
+			l++
+		}
+		// 从 pivot 的右边边找到小于等于pivot的值
+		for array[r] > pivot {
+			r--
+		}
+		// 1 >= r 表明本次分解任务完成, break
+		if l >= r {
+			break
+		}
+		// 交换
+		temp = array[l]
+		array[l] = array[r]
+		array[r] = temp
+		// 优化
+		if array[l] == pivot {
+			r--
+		}
+		if array[r] == pivot {
+			l++
+		}
+	}
+
+	// 如果  1== r, 再移动下
+	if l == r {
+		l++
+		r--
+	}
+
+	// 向左递归
+	if left < r {
+		QuickSort(left, r, array)
+	}
+
+	// 向右递归
+	if right > l {
+		QuickSort(l, right, array)
+	}
+}
+
+func main() {
+	// arr := [9]int {-9,78,0,23,-567,70, 123, 90, -23}
+	// fmt.Println("初始arr值", arr)
+	// 调用快速排序
+	// QuickSort(0, len(arr) - 1, &arr)
+	// fmt.Println("结束arr值", arr)
+
+	var arr [80000000]int
+	for i := 0; i < 80000000; i++ {
+		arr[i] = rand.Intn(9000000)
+	}
+	start := time.Now().Unix()
+	QuickSort(0, len(arr)-1, &arr)
+	end := time.Now().Unix()
+	fmt.Printf("快速排序法耗时%d秒", end-start)
+}
